615b590971e738a0585ad5d3518432996b4df214
PratamaAngga <anggashillnoir@gmail.com>
2024-11-18 11:19:45 +0700

    Experiment 3 after question

--- Experiment/ArrayAverageScore20.java
+++ Experiment/ArrayAverageScore20.java
@@ -3,18 +3,29 @@ import java.util.Scanner;
 public class ArrayAverageScore20 {
     public static void main(String[] args) {
        Scanner sc = new Scanner(System.in);
-        int score[] = new int[10];
-        double total = 0;
-        double average;
+       System.out.print("Input the number of student : ");
+       int students = sc.nextInt();
+        int score[] = new int[students];
+        double totalPassed = 0, totalFailed = 0;
+        double averagePassed, averageFailed;
+        int studentPassed = 0, studentFailed = 0;
 
         for (int i = 0; i < score.length; i++) {
             System.out.print("Enter student score " + (i + 1) + " : ");
             score[i] = sc.nextInt();
         }
         for (int i = 0; i < score.length; i++) {
-            total += score[i];
+            if (score[i] > 70) {
+                totalPassed += score[i];
+                studentPassed++;
+            } else if (score[i] <= 70) {
+                totalFailed += score[i];
+                studentFailed++;
+            }
         }
-        average = total / score.length;
-        System.out.println("The class average score is " + average);
+        averagePassed = (studentPassed > 0) ? totalPassed /studentPassed : 0;
+        averageFailed = (studentFailed > 0) ? totalFailed /studentFailed : 0;
+        System.out.println("The average score of students who passed is " + averagePassed);
+        System.out.println("The average score of students who failed is " + averageFailed);
     }
 }
